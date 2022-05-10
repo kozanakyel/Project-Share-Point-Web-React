7f398906d8517a5aed6f75f289621b42bf38f625
kozanakyel <kozanakyel@gmail.com>
2022-05-11 02:33:56 +0300

    configure index.js for react 18 upgrade

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './utils/AuthProvider';
 
-ReactDOM.render(
+const container =   document.getElementById('app');
+const root = createRoot(container);
+
+root.render(
     <BrowserRouter>
         <AuthProvider>
             <Routes>
                 <Route path="/*" element={<App />} />
             </Routes>
         </AuthProvider>    
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 );
 
 
