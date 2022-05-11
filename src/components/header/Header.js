197f9350d90b2aa9ceda3eab760039a6aea843ab
kozanakyel <kozanakyel@gmail.com>
2022-05-12 00:29:25 +0300

    finding appropriate card for projects

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ProjectCard from '../projectcard/ProjectCard';
 
 const Header = () => {
     return <>
         <div class="p-5 text-center text-success bg-dark" style={{ marginTop:'0px' }}>
             <h1 class="mb-3">Project Share Point</h1>
             <h4 class="mb-3">Would you like to share your every situation and thought with us? continue to see latest projects</h4>
-            <a class="btn btn-success" href="#!" role="button">Let See Projects</a>
+            <Link class="btn btn-success" to="/allprojects" role="button">Let See Projects</Link>
+            
         </div>
     </>
 }
---
+++ src/components/projectcard/ProjectCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import img1 from '../../samples/rasp.jpg';
+
+const ProjectCard = () => {
+  return <>
+   </>
+}
+
+export default ProjectCard;
